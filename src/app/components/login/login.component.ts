3a5ebc0133c726af24b64261b53001b1deb4a8ad
oscarvx00 <oscarvicente@usal.es>
2022-10-12 13:02:56 +0200

    Login completed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,11 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  loginWithProvider(providerName : string){
+    window.open(environment.backendEndpoint + `/oauth2/authorization/${providerName}`)
   }
 
 }
